528a1b43de65d29a40c5adbdc91cdc87cf2a7f03
Roberto Acevedo <robante12@gmail.com>
2022-06-25 18:00:27 -0600

    Horizontal Files Load

--- gui/mainWindowLogic.py
+++ gui/mainWindowLogic.py
@@ -5,6 +5,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 
 from gui.mainWindow import Ui_MainWindow
+from src.img_splitter import IMGSplitter
 from src.file_utils import FileUtils
 
 working_directory: str = ''
@@ -19,11 +20,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.statusbar.showMessage('Preparado')
         self.btn_openFolder.clicked.connect(self.OpenFolder)
         self.btn_loadFolderFiles.clicked.connect(self.ScanFolderFiles)
-        self.table_filesList.itemClicked.connect(self.TableClickEvent)
+        self.table_filesList.itemClicked.connect(self.TablePreviewClickEvent)
+        self.table_horizontalList.itemClicked.connect(self.TableHorizontalClickEvent)
         self.btn_sortTableRise.clicked.connect(self.moveUp)
         self.btn_sortTableDescend.clicked.connect(self.moveDown)
         self.btn_sortTableDelete.clicked.connect(self.deleteRow)
-        self.btn_loadHorizontal.clicked.connect(self.selectedFilesArray)
+        self.btn_loadHorizontal.clicked.connect(self.loadHorizontalFiles)
 
     def OpenFolder(self):
         folder = str(QFileDialog.getExistingDirectory(
@@ -36,35 +38,44 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if self.txt_inputFolder.text() != '':
             fUtils = FileUtils()
             files_array = fUtils.readFolderFiles(self.txt_inputFolder.text())
-            self.cleanTable()
-            for file in files_array:
-                row = self.table_filesList.rowCount()
-                self.table_filesList.setRowCount(row+1)
-                cell = QTableWidgetItem(str(file))
-                self.table_filesList.setItem(row, 0, cell)
+            self.cleanTable(self.table_filesList)
+            self.fillTable(files_array, self.table_filesList)
         else:
             self.statusbar.showMessage(
                 'Error: Primero debes de seleccionar un directorio')
 
-    def TableClickEvent(self):
+    def TablePreviewClickEvent(self):
         row = self.table_filesList.currentRow()
         image_path = self.table_filesList.item(row, 0).text()
         global working_directory
-        self.loadPreview(working_directory+'/'+image_path)
+        self.loadPreview(working_directory+'/'+image_path, self.gfx_Preview)
 
-    def loadPreview(self, image_path: str):
+    def TableHorizontalClickEvent(self):
+        row = self.table_horizontalList.currentRow()
+        image_path = self.table_horizontalList.item(row, 0).text()
+        global working_directory
+        self.loadPreview(working_directory+'/'+image_path, self.gfx_splitPreview)
+
+    def loadPreview(self, image_path: str, gfx_viewer: QGraphicsView):
         if os.path.isfile(image_path):
             scene = QtWidgets.QGraphicsScene(self)
             pixmap = QPixmap(image_path)
             item = QtWidgets.QGraphicsPixmapItem(pixmap)
             scene.addItem(item)
-            self.gfx_Preview.setScene(scene)
-            self.gfx_Preview.fitInView(
+            gfx_viewer.setScene(scene)
+            gfx_viewer.fitInView(
                 scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
 
-    def cleanTable(self):
-        while (self.table_filesList.rowCount() > 0):
-            self.table_filesList.removeRow(0)
+    def cleanTable(self, table: QTableWidget):
+        while (table.rowCount() > 0):
+            table.removeRow(0)
+
+    def fillTable(self, filesArray: list, table: QTableWidget):
+        for file in filesArray:
+            row = table.rowCount()
+            table.setRowCount(row+1)
+            cell = QTableWidgetItem(str(file))
+            table.setItem(row, 0, cell)
 
     def moveDown(self):
         row = self.table_filesList.currentRow()
@@ -101,3 +112,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         for i in range(self.table_filesList.rowCount()):
             selectedFiles.append(self.table_filesList.item(i, 0).text())
         return selectedFiles
+
+    def loadHorizontalFiles(self):
+        selectedFiles = self.selectedFilesArray()
+        HorizontalFiles = []
+        global working_directory
+        for file in selectedFiles:
+            orientation = IMGSplitter.check_orientation(
+                working_directory + '/' + file)
+            if orientation == 'H':
+                HorizontalFiles.append(file)
+        self.fillTable(HorizontalFiles, self.table_horizontalList)
+
+    def splitFilesAction(self):
+        horizontalFiles = []
+        for i in range(self.table_filesList.rowCount()):
+            horizontalFiles.append(self.table_filesList.item(i, 0).text())
+        return horizontalFiles
